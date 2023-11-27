ff33643466d514b5c497708dee658b43a7d14731
Gian Joker <70668328+JokerGt007@users.noreply.github.com>
2023-11-27 19:44:53 -0300

    Add files via upload

--- aqua/app/views.py
+++ aqua/app/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-from . models import *
+from .models import *
 
 def consulta(request):
-    ecossistemas = Ecossistemas.objects.all()
-    animais = Animais.objects.all()
-    racas = Raca.objects.all()
-    especies = Especie.objects.all()
-    
+    ecossistemas = Ecossistemas.objects.prefetch_related('animais_set').all()
+
     consultas = {
         'ecossistemas': ecossistemas,
-        'animais': animais,
-        'racas': racas,
-        'especies': especies
     }
-    
+
     return render(request, 'consulta/consulta.html', consultas)
 
 def quiz(request):
